ac8b05e65c28cf990c68ca9d95783779da8aeaca
apetbrz <apetbrz@gmail.com>
2025-01-03 17:20:00 -0500

    inserted (hidden) sidebar menu, needs menu button to animate

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css'
 import Relics from "./components/Relics.jsx"
+import Menu from "./components/Menu.jsx"
 
 function App() {
     return (
         <>
+        <Menu />
         <h1 className="bold">RelicLookup</h1>
         <Relics />
         </>
---
+++ client/src/components/Menu.jsx
@@ -0,0 +1,12 @@
+import './Menu.css'
+
+function Menu() {
+    return (
+        <div className="menu">
+            <a href="/">test</a>
+            <a href="/">test</a>
+        </div>
+    )
+}
+
+export default Menu
